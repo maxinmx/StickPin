7b74febbcc52256c308e856b0d370d9d6aa4b727
maxin <mx1759808857@163.com>
2018-03-31 19:42:44 +0800

    针的控制
    针到达位置及连环发射

---
+++ StickPinProject/Assets/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    private Transform startPoint;
+    private Transform spawnPoint;
+    private Pin currentPin;
+
+    public GameObject pinPrefab;
+
+
+	// Use this for initialization
+	void Start () {
+        startPoint = GameObject.Find("StartPoint").transform;
+        spawnPoint = GameObject.Find("SpawnPoint").transform;
+        SpawnPin();
+	}
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            currentPin.StartFly();
+            SpawnPin();
+        }
+    }
+
+    void SpawnPin()//instance a pin
+    {
+        currentPin=GameObject.Instantiate(pinPrefab, spawnPoint.position, pinPrefab.transform.rotation).GetComponent<Pin>();
+    }
+}
---
+++ StickPinProject/Assets/Scripts/Pin.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    public float speed = 5;
+
+    private bool isFly = false;
+    private bool isReach = false;
+    private Transform startPoint;
+    private Transform circle;
+
+    private Vector3 targetCirclePos;
+
+	// Use this for initialization
+	void Start () {
+        startPoint = GameObject.Find("StartPoint").transform;
+        circle = GameObject.Find("Circle").transform;
+        targetCirclePos = circle.position;
+        targetCirclePos.y -= 1.45f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isFly == false)//not reach circle
+        {
+            if (isReach == false)//not reach startPoint
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startPoint.position, speed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, startPoint.position) < 0.05f)
+                {
+                    isReach = true;
+                }
+            }
+           //if reach startPoint ,nothing to do 
+        }
+        else//reach circle
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetCirclePos, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, targetCirclePos) < 0.05f)
+            {
+                transform.position = targetCirclePos;
+                transform.parent = circle;
+                isFly = false;
+            }
+        }
+	}
+
+    public void StartFly()
+    {
+        isFly = true;
+        isReach = true;
+    }
+}
